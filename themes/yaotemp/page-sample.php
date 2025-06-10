339cc0bf65b8f38248d1fe8b29aa435b48571fb8
kishi1997 <tomo9634@gmail.com>
2025-06-10 13:14:54 -0700

    サンプルページを新規作成し、各種画像とコンセプトに関する情報を追加しました。また、SVG画像やJPEG画像を新たに追加し、デザイン要素を強化しました。

---
+++ themes/yaotemp/page-sample.php
@@ -0,0 +1,175 @@
+<?php get_header(); ?>
+<div class="sample">
+    <section class="c-lowerPage-mv">
+        <div class="c-lowerPage-mv__ttl">
+            <span>サンプルページ</span>
+            <div class="c-lowerPage-mv__ttl__desc">
+                <h1><?php the_title(); ?></h1>
+            </div>
+        </div>
+    </section>
+    <p>流用できるパーツがあれば参考にしてください</p>
+    <section class="sample-feature">
+        <div class="l-container">
+            <div class="sample-feature__content">
+                <div class="sample-feature__item">
+                    <div class="sample-feature__item__text">
+                        <h3 class="sample-feature__item__ttl"><span>Woven Ceramic Echizen</span><br />瓦で織る壁</h3>
+                        <div>
+                            <p>私は北海道の釧路生まれで小さい時親父を亡くし、母親の再婚に伴い新しい親父に育てられ、
+                                中学校までは一度も北海道を出る事も無く育ってきました。
+                                野球が好きで親の応援もあり、高校は奈良の天理高校へと進学しました。 皆さんも御存じの通り、奈良には日本を代表する古建築や文化財が多数あります。
+                                日本最古と言われる法隆寺（築1415年）や東大寺（築1267年）など、そんな奈良の建物に魅力を感じ休みの時は知らず知らずの間に古い建物や民家を見て歩くようになりました。
+                                いつしか建築に携わる仕事に就きたいと思うようになり、高校を無事卒業し、北海道に帰り旭川にある建築の専門学校へ進学しました。
+                                （ちなみに野球の事については触れないで下さい、あしからず。（笑））
+                                その頃から一人前の大工になりいずれは棟梁として将来やって行く目標が出来、卒業後芦野組株式会社へ入社し今まで30年大工として頑張ってきました。</p>
+                            <p>確かに奈良県や京都・滋賀がある近畿圏は、歴史的な木造建築物があちこちにある伝統的建造物群保存地区として、建物だけじゃなく日本の文化や生活様式を残していこうという考えがありますよね。その町すべてを若い世代に継承していこう、という。
+                                近江八幡の京街道沿いや奈良県の今井町、京都などは、昔の町の雰囲気を求めてたくさんの観光客が訪れます。それは、街並みを形成している古い建物が文化的な背景を匂わせ、人を惹きつけるからだと思うんです。
+                                本当の家創りは、流行り廃りとは正反対で、子や孫の代まで受け継がれるものだと感じています。歴史が息づく地域に住んでいる僕らが守るべき、日本の家。それが『しげや』ブランドのコンセプトなんです。
+                            </p>
+                        </div>
+                    </div>
+                    <div class="sample-feature__item__img"><img alt="デザインホームイメージ１"
+                            src="<?php bloginfo('template_url');?>/img/sample/sample-feat-img1.jpg" /><img
+                            alt="デザインホームイメージ２"
+                            src="<?php bloginfo('template_url');?>/img/sample/sample-feat-img2.jpg" /></div>
+                </div>
+                <div class="sample-feature__text__w100">
+                    <p>北海道の建物と近畿圏の建物には大きな違いがあって、長年、北海道で建てさせてもらった建物は殆どが松や杉の木材なんですよ。近畿圏で手に入る桧や杉は殆ど北海道では手に入らないんです。
+                        私も荒川建設さんにお世話になり吉村棟梁の下で教わりましたが、最初の頃はビックリしましたよ。
+                        現場に運び込まれる東濃檜の多さ、屋根のタル木や本物の無垢の床材。何と言ってもカウンター・机・テーブル・洗面台・本棚の棚板一枚に至るまで本物の木で造り上げていく醍醐味は、本当に大工になって良かったと実感しています。
+                        確かに手間と時間は掛かりますが、規格型住宅を建てているかちわり大工では難しいですよね！ だから1軒1軒違う和の家が出来るんだと思います。</p>
+                </div>
+                <div class="sample-feature__item">
+                    <div class="sample-feature__item__img">
+                        <img alt="デザインホームイメージ１"
+                            src="<?php bloginfo('template_url');?>/img/sample/sample-feat-img1.jpg" /><img
+                            alt="デザインホームイメージ２"
+                            src="<?php bloginfo('template_url');?>/img/sample/sample-feat-img2.jpg" />
+                    </div>
+                    <div class="sample-feature__item__text">
+                        <h3 class="sample-feature__item__ttl"><span>未来へ伝える、本物の技と心意気</span><br />— Real Technique,
+                            Real Spirit</h3>
+                        <div>
+                            <p>建てる側の人間がそういった気持ちで建てていれば、すべてのお施主様が喜んでくださると思っています。ただ、今の時代それが古いと言われればそれまでです。確かに、現場で組み上げるだけのプレハブに、内壁のボードにクロスを貼るだけの仕上げなら、住める家を早く作ることはできる。でも、日本人が住む家がそれでいいんでしょうか。
+                                先々代の時代から、素晴らしい技をもつ職人さんたちも跡取りがいないと嘆いていました。時代の流れで済ますにはあまりに惜しいので、手の良い職人さん達が次の時代へも引き継がれるような日本の住宅市場になることを願っています。
+                                自然素材って、本当に健康にもいいんですよ！私は三栖さんの元で10年大工修行を積み、しげやの家の魅力を日々感じていました。もともと身体が弱かったのですが、自然素材と木の香りいっぱいの現場ですごく健康になりましたしね。今はしげやの広報として、たくさんの方にその素晴らしさを伝えたいと思っています。
+                                今、私は充実しています。
+                                先代に続きお二人と一緒に仕事をさせて頂き、現場でお施主様と共に向き合って末長く住んで頂く家が建てられる事に・・・。
+                                お施主様に感謝・感謝するばかりです。縁あってこうして仕事をご一緒している多くの職人さんや業者さん、お施主様の応援があってこその『しげや』です。この『しげや』というブランドが他の工務店にも元気を与えるような存在になれればいいなと思っています。そして何より、「やっぱり家創りは楽しい！」って皆さんに感じていただきたいですね。そのお手伝いをできることに感謝しています。
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- point -->
+    <section class="sample-point">
+        <div class="l-container">
+            <div class="sample-point__content">
+                <div class="sample-point__item "><img alt="「完全オーダーメイドで理想の暮らしを実現」イメージ画像"
+                        src="<?php bloginfo('template_url');?>/img/sample/sample-point-img1.jpg" />
+                    <div><span class="deco-number"><img alt="01"
+                                src="<?php bloginfo('template_url');?>/img/sample/bg-number-01.svg" /></span>
+                        <h3 class="sample-point__item__ttl">サンプルタイトル</h3>
+                        <p>ご家族のライフスタイルに合わせた、個々の動き・活動・空間にフィットする住まいを提案します。</p>
+                        <p>決まった形はありません。お客様の想いを大切にし、素敵な住まいを実現するお手伝いをします。</p>
+                    </div>
+                </div>
+            </div>
+            <div class="sample-point__content">
+                <div class="sample-point__item sample-point__item__rev">
+                    <div><span class="deco-number"><img alt="02"
+                                src="<?php bloginfo('template_url');?>/img/sample/bg-number-02.svg" /></span>
+                        <h3 class="sample-point__item__ttl">サンプルタイトル</h3>
+                        <p>理想の家を実現するためには、敷地の特性を活かすことが重要です。お客様のライフスタイルに合わせた最適なプランをご提案します。</p>
+                        <p>また、周辺環境や将来的な変化にも配慮したプランニングを行い、安心して住める家づくりをサポートします。</p>
+                    </div><img alt="「敷地条件を活かした柔軟なプランニング」イメージ画像"
+                        src="<?php bloginfo('template_url');?>/img/sample/sample-point-img2.jpg" />
+                </div>
+            </div>
+            <div class="sample-point__content">
+                <div class="sample-point__item "><img alt="「自然素材と最新技術の融合」イメージ画像"
+                        src="<?php bloginfo('template_url');?>/img/sample/sample-point-img3.jpg" />
+                    <div><span class="deco-number"><img alt="03"
+                                src="<?php bloginfo('template_url');?>/img/sample/bg-number-03.svg" /></span>
+                        <h3 class="sample-point__item__ttl">サンプルタイトル</h3>
+                        <p>無垢材や漆喰といった自然素材の魅力を活かしながら、断熱性能・耐震構造・省エネ設計など、現代の住宅に必要な性能も確保。</p>
+                        <p>見た目の美しさと暮らしの快適性、どちらも兼ね備えたバランスの良い住まいを実現します。安心して長く住めることを大切にしています。</p>
+                    </div>
+                </div>
+            </div>
+            <p>4以降の画像</p>
+            <div class="l-cotainer">
+                <img alt="04" src="<?php bloginfo('template_url');?>/img/sample/bg-number-04.svg" />
+                <img alt="05" src="<?php bloginfo('template_url');?>/img/sample/bg-number-05.svg" />
+                <img alt="06" src="<?php bloginfo('template_url');?>/img/sample/bg-number-06.svg" />
+            </div>
+        </div>
+    </section>
+    <section class="concept-2col">
+        <div class="l-container">
+            <div class="concept-2col__content">
+                <div class="concept-2col___item">
+                    <h3><span>01
+                            -
+                        </span>耐震構造に優れた家づくり</h3><img alt="「耐震構造に優れた家づくり」イメージ"
+                        src="<?php bloginfo('template_url');?>/img/sample/concept-menu-img1.jpg" />
+                    <p> 地震に強い家を作るため、私たちは実績や最新の技術を駆使した設計に取り組んでいます。高耐震設計で安心、安全のプラン設計を行い、耐震リフォームも提供。万全の耐震性を確保しながら、自然な暮らしを守る家づくりを実現します。
+                    </p>
+                </div>
+                <div class="concept-2col___item">
+                    <h3><span>02
+                            -
+                        </span>子育て世代も納得のキッズデザイン</h3><img alt="「子育て世代も納得のキッズデザイン」イメージ"
+                        src="<?php bloginfo('template_url');?>/img/sample/concept-menu-img2.jpg" />
+                    <p>子どもたちが安全に過ごせる環境を考えた家づくりが、私たちの使命です。家族全員が安心して暮らせる空間を提供するために、地球に優しい素材を使用し、子どもの成長も見守れるデザインを採用しています。
+                    </p>
+                </div>
+                <div class="concept-2col___item">
+                    <h3><span>03
+                            -
+                        </span>万全のアフターメンテナンス</h3><img alt="「万全のアフターメンテナンス」イメージ"
+                        src="<?php bloginfo('template_url');?>/img/sample/concept-menu-img3.jpg" />
+                    <p>私たちは、家づくりが終わった後もお客様を支える体制をしっかりと整えています。定期的な点検やメンテナンスを行い、住まいの健全を保つためのサポートを提供。お客様の住まいが安心で快適なままで過ごせるように継続的にサポートします。
+                    </p>
+                </div>
+                <div class="concept-2col___item">
+                    <h3><span>04 -
+                        </span>日本最大級のLIXILグループの一員</h3><img alt="「日本最大級のLIXILグループの一員」イメージ"
+                        src="<?php bloginfo('template_url');?>/img/sample/concept-menu-img4.jpg" />
+                    <p>アイフルホームは、LIXILグループの一員として、住まいづくりにおいて国内外の支援を受けています。LIXILが持つ豊富な資源とグローバルなネットワークを活かし、厳選した高品質な建材や最新技術を取り入れています。
+                    </p>
+                </div>
+            </div>
+
+        </div>
+    </section>
+    <div class="btn-parts">
+        <div class="l-container">
+            <h3>btn</h3>
+            <a href="<?php echo esc_url($item['button_link']); ?>" class="c-btn">
+                <span class="c-btn__text">text</span>
+                <span class="c-btn__icon">
+                    <svg width="21" height="8" viewBox="0 0 21 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M20.4276 4.35355C20.6229 4.15829 20.6229 3.84171 20.4276 3.64645L17.2456 0.464466C17.0503 0.269204 16.7338 0.269204 16.5385 0.464466C16.3432 0.659728 16.3432 0.976311 16.5385 1.17157L19.3669 4L16.5385 6.82843C16.3432 7.02369 16.3432 7.34027 16.5385 7.53553C16.7338 7.7308 17.0503 7.7308 17.2456 7.53553L20.4276 4.35355ZM0.0740356 4V4.5H20.074V4V3.5H0.0740356V4Z"
+                            fill="<?php echo $primary_color;  ?>" />
+                    </svg>
+                </span>
+            </a>
+            <a href="<?php echo esc_url($item['button_link']); ?>" class="c-btn c-btn--primary">
+                <span class="c-btn__text">text</span>
+                <span class="c-btn__icon">
+                    <svg width="21" height="8" viewBox="0 0 21 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M20.4276 4.35355C20.6229 4.15829 20.6229 3.84171 20.4276 3.64645L17.2456 0.464466C17.0503 0.269204 16.7338 0.269204 16.5385 0.464466C16.3432 0.659728 16.3432 0.976311 16.5385 1.17157L19.3669 4L16.5385 6.82843C16.3432 7.02369 16.3432 7.34027 16.5385 7.53553C16.7338 7.7308 17.0503 7.7308 17.2456 7.53553L20.4276 4.35355ZM0.0740356 4V4.5H20.074V4V3.5H0.0740356V4Z"
+                            fill="<?php echo $primary_color;  ?>" />
+                    </svg>
+                </span>
+            </a>
+        </div>
+    </div>
+</div>
+<?php get_footer(); ?>
